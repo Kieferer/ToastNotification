1732a867445bc0af8710b1a2d5a2f4f45ca365dd
Kieferer <balazsborsi0@gmail.com>
2023-09-26 17:14:04 +0200

    feat: Add 'SnackbarProvider' component to 'App.tsx' body and set the limit the max amount of rendered notifications to 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import {SnackbarProvider} from "notistack";
 
 function App() {
 
   return (
     <>
+      <SnackbarProvider maxSnack={5}/>
       <p className={'content-place-holder'}>
         Content of the web page
       </p>
