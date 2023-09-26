bddc322848a2506e549f8d217b114a3255bb7bcd
Kieferer <balazsborsi0@gmail.com>
2023-09-26 17:40:12 +0200

    feat: Create basic implementation for 'ToastNotification', implement UseEffect which setup and create the notification and close it after 5 seconds of creation

--- src/notifications/ToastNotification.tsx
+++ src/notifications/ToastNotification.tsx
@@ -1,4 +1,27 @@
+import {useEffect} from "react";
+import {useSnackbar} from "notistack";
+
 const ToastNotification = () => {
+
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+
+  useEffect(() => {
+    const message = 'Welcome, user!';
+    const key = enqueueSnackbar(message, {
+      variant: 'default',
+      action: (key) => (
+        <>
+          <button onClick={() => closeSnackbar(key)}>{'✖'}</button>
+        </>
+      ),
+    });
+
+    setTimeout(() => {
+      closeSnackbar(key);
+    }, 5000);
+
+  }, [enqueueSnackbar, closeSnackbar]);
+
   return (
     <></>
   )
