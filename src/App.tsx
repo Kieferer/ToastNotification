0f5b1ef8140bf0a00ceb971950e8d5cc859e0a31
Kieferer <balazsborsi0@gmail.com>
2023-09-27 13:50:22 +0200

    refactor: Clear 'NotiStack' specific implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import './App.css'
-import {SnackbarProvider} from "notistack";
-import ToastNotification from "./notifications/ToastNotification.tsx";
 
 function App() {
 
   return (
     <>
-      <SnackbarProvider maxSnack={5}>
-        <ToastNotification/>
-      </SnackbarProvider>
       <p className={'content-place-holder'}>
         Content of the web page
       </p>
