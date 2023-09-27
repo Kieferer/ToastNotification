f83b5b6aff32fdf7f410c9ecab61f841d5c3f145
Kieferer <balazsborsi0@gmail.com>
2023-09-27 12:00:23 +0200

    feat: Set style of 'ToastNotification' with inline styling

--- src/notifications/ToastNotification.tsx
+++ src/notifications/ToastNotification.tsx
@@ -1,6 +1,5 @@
 import {useEffect} from "react";
 import {useSnackbar} from "notistack";
-
 const ToastNotification = () => {
 
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
@@ -18,6 +17,15 @@ const ToastNotification = () => {
       anchorOrigin: {
         vertical: 'top',
         horizontal: 'right'
+      },
+      style: {
+        backgroundColor: "#271811",
+        height: "90px",
+        width: "342px",
+        borderTopLeftRadius: '25px',
+        borderTopRightRadius: '25px',
+        borderBottomLeftRadius: '25px',
+        borderBottomRightRadius: '25px',
       }
     });
 
