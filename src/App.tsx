679fa79369afb791c4a4bf075a1f94e55d87290e
Kieferer <balazsborsi0@gmail.com>
2023-09-27 18:55:55 +0200

    feat: Add 'ToastNotification' component to HTML skeleton and set alignment of placeholder paragraph center

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
+import ToastNotification from "./assets/notification/ToastNotification.tsx";
 
 function App() {
 
   return (
     <>
-      <p className={'content-place-holder'}>
+      <p className={'text-center'}>
         Content of the web page
       </p>
+      <ToastNotification message={"Welcome"} highlightedMessage={"username"}/>
     </>
   )
 }
